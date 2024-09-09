98ea5995e3478337727354b5c6a2703a510e6c1f
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2024-09-09 09:52:10 -0400

    first commit

---
+++ LinkedList/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        Integer a = new Integer(5);
+        Integer b = new  Integer(7);
+
+        Integer c;
+        c = a;
+
+        System.out.println(c);
+    }
+}
