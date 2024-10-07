75134f9d3c4be26fd1940fd0dc7951ce07ae3a00
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2024-10-07 13:47:29 -0400

    Add initial flights problem

---
+++ Stacks/Flights.java
@@ -0,0 +1,20 @@
+package Stacks;
+
+public class Flights {
+    public static void main(String[] args) throws  {
+        Scanner scan = new Scanner(new File("flights.txt"));
+
+        ArrayList<String> flightletter = new ArrayList<>();
+
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            flightletter.add(line);
+        }
+
+        //We start at T , 
+
+        
+
+        
+    }
+}
