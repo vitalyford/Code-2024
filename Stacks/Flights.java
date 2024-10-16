3c41b489906704288a00b04f46a2e11531be88d1
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2024-10-16 12:05:40 -0400

    Complete flights problem using stack and arraylist

--- Stacks/Flights.java
+++ Stacks/Flights.java
@@ -1,7 +1,10 @@
-package Stacks;
+package Tests;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class Flights {
@@ -16,18 +19,36 @@ public class Flights {
         // TODO: add a break to break the loop
         // if we didn't reach the dst
         while (!stack.peek().equals(dst)) {
-            // Go to the next available place
+            boolean foundNextCity = false;
+            // Go to the next available place/city
             for (String path : flightLetter) {
-                if (path.substring(0, 1).equals(stack.peek()) && 
+                if (path.substring(0, 1).equals(stack.peek()) &&
                         !visited.contains(path.substring(2))) {
                     stack.push(path.substring(2));
                     visited.add(path.substring(2));
+                    foundNextCity = true;
+                    break;
                 }
             }
+            if (!foundNextCity) {
+                stack.pop();
+
+            }
+            if (stack.isEmpty()) {
+                System.out.println("Go Walking. --Jefferson");
+                return;
+            }
         }
+
+        String out = "";
+
+        while (!stack.isEmpty()) {
+            out = stack.pop() + " -> " + out;
+        }
+        System.out.println(out);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new File("flights.txt"));
 
         ArrayList<String> flightletter = new ArrayList<>();
@@ -40,12 +61,14 @@ public class Flights {
         scan.close();
 
         scan = new Scanner(System.in);
-        System.out.print("Give me the src and dst: ");
-        String[] line = scan.nextLine().split(" ");
-        String src = line[0];
-        String dst = line[1];
+        while (true) {
+            System.out.print("Give me the src and dst: ");
+            String[] line = scan.nextLine().split(" ");
+            String src = line[0];
+            String dst = line[1];
 
-        // Ready to implement the algorithm
-        searchPath(src, dst, flightletter);
+            // Ready to implement the algorithm
+            searchPath(src, dst, flightletter);
+        }
     }
 }
