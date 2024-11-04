7ac1d388c412c280aca099349d6f071fb2f3cc7a
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2024-11-04 10:19:03 -0500

    Finalize flood problem solution

--- Queues/Flood.java
+++ Queues/Flood.java
@@ -5,9 +5,10 @@ class Point {
     int y;
     int step;
 
-    public Point(int x, int y) {
+    public Point(int x, int y, int step) {
         this.x = x;
         this.y = y;
+        this.step = step;
     }
 
     @Override
@@ -17,7 +18,7 @@ class Point {
     }
 
     public Point plus(Point p) {
-        return new Point(x + p.x, y + p.y);
+        return new Point(x + p.x, y + p.y, step + p.step);
     }
 }
 
@@ -25,18 +26,19 @@ public class Flood {
     // a starting maze 10x10 to flood with water represented by a 2D array
     // 0 represents a dry cell and 1 represents a cell with water
     public static int[][] floor = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 1, 0, 0, 1, 1, 1, 0, 1, 0 },
-            { 0, 1, 1, 0, 0, 1, 0, 0, 1, 0 },
-            { 0, 0, 1, 0, 0, 1, 0, 0, 1, 1 },
-            { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
-            { 0, 0, 1, 1, 1, 0, 1, 0, 0, 0 },
-            { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 },
-            { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 } };
+                                    { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                    { 0, 1, 0, 0, 1, 1, 1, 0, 1, 0 },
+                                    { 0, 1, 1, 0, 0, 1, 0, 0, 1, 0 },
+                                    { 0, 0, 1, 0, 0, 1, 0, 0, 1, 1 },
+                                    { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                                    { 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+                                    { 0, 0, 1, 1, 1, 0, 1, 0, 0, 0 },
+                                    { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 },
+                                    { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 } };
 
     public static void main(String[] args) {
-        Point start = new Point(7, 3);
+        // new Point(start x, start y, steps count)
+        Point start = new Point(7, 3, 0);
         ArrayList<Point> visited = new ArrayList<>();
 
         Queue<Point> q = new LinkedList<>();
@@ -44,28 +46,32 @@ public class Flood {
         visited.add(start);
 
         ArrayList<Point> dirs = new ArrayList<>();
-        dirs.add(new Point(-1, 0)); // go left
-        dirs.add(new Point(0, -1)); // go up
-        dirs.add(new Point(1, 0)); // go right
-        dirs.add(new Point(0, 1)); // go down
+        dirs.add(new Point(-1, 0, 1)); // go left
+        dirs.add(new Point(0, -1, 1)); // go up
+        dirs.add(new Point(1, 0, 1)); // go right
+        dirs.add(new Point(0, 1, 1)); // go down
 
         while (!q.isEmpty()) {
             Point curr = q.poll();
+            visited.add(curr);
             
             for (Point dir : dirs) {
                 Point nextP = curr.plus(dir);
-                visited.add(nextP);
                 // 1. if we cannot go there, do not go there
                 if (nextP.x >= floor[0].length
                         || nextP.x < 0
                         || nextP.y >= floor.length
                         || nextP.y < 0
-                        || floor[nextP.y][nextP.x] > 0) {
+                        || floor[nextP.y][nextP.x] > 0
+                        || visited.contains(nextP)) {
                     continue;
                 }
                 // the following lines will only hit if
                 // we can go to that next point
                 q.offer(nextP);
+            }
+            if (q.isEmpty()) {
+                System.out.println(curr.step);
             }
         }
     }
